2526869c6a6b6a4505249a770429e756b4f6a292
Admin <deshwalbadal007@gmail.com>
2026-08-12 16:55:38 +0530

    Fix orders API imports

--- src/redux/features/orders/ordersApi.js
+++ src/redux/features/orders/ordersApi.js
@@ -1,32 +1,38 @@
  import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import getBaseUrl from "../../../utils/baseURL";
-
+import getBaseUrl from "../../../utils/baseUrl";
 
 const ordersApi = createApi({
-    reducerPath: 'ordersApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${getBaseUrl()}/api/orders`,
-        credentials: 'include'
-    }),
-    tagTypes: ['Orders'],
-    endpoints: (builder) => ({
-        createOrder: (builder.mutation) ({
-            query: (newOrder) => ({
-                url: "/",
-                method: "POST",
-                body: newOrder,
-                credentials: 'include',
-            })
-        }),
-        getOrderByEmail: (builder.query) ({
-            query: (email) => ({
-                url: `/email/${email}`
-            }),
-            providesTags: ['Orders']
-        })
-    })
-})
+  reducerPath: "ordersApi",
 
-export const {useCreateOrderMutation, useGetOrderByEmailQuery} = ordersApi;
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${getBaseUrl()}/api/orders`,
+    credentials: "include",
+  }),
+
+  tagTypes: ["Orders"],
+
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (newOrder) => ({
+        url: "/",
+        method: "POST",
+        body: newOrder,
+        credentials: "include",
+      }),
+    }),
+
+    getOrderByEmail: builder.query({
+      query: (email) => ({
+        url: `/email/${email}`,
+      }),
+      providesTags: ["Orders"],
+    }),
+  }),
+});
+
+export const {
+  useCreateOrderMutation,
+  useGetOrderByEmailQuery,
+} = ordersApi;
 
 export default ordersApi;

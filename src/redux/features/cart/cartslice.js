e0cbd3fcee7450fb2c1b9772a76739114ca4d57b
Admin <deshwalbadal007@gmail.com>
2026-08-12 17:27:47 +0530

    Fix cart slice syntax and imports

--- src/redux/features/cart/cartslice.js
+++ src/redux/features/cart/cartslice.js
@@ -1,46 +1,59 @@
  import { createSlice } from "@reduxjs/toolkit";
-import Swal  from "sweetalert2";
+import Swal from "sweetalert2";
 
 const initialState = {
-    cartItems: []
-}
+  cartItems: [],
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: initialState,
-    reducers:{
-        addToCart: (state, action) => {
-            const existingItem = state.cartItems.find(item => item._id === action.payload.id);
-            if(!existingItem) {
-                state.cartItems.push(action.payload)
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "Product Added to the Cart",
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-            } else(
-                Swal.fire({
-                    title: "Already Added to the Cart",
-                    text: "You won't be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#3085d6",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "OK!"
-                  })
-            )
-        },
-        removeFromCart: (state, action) => {
-            state.cartItems =  state.cartItems.filter(item => item._id !== action.payload._id)
-        },
-        clearCart: (state) => {
-            state.cartItems = []
-        }
-    }
-})
+  name: "cart",
+  initialState,
 
-// export the actions   
-export const  {addToCart, removeFromCart, clearCart} = cartSlice.actions;
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+
+      if (!existingItem) {
+        state.cartItems.push(action.payload);
+
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Product Added to the Cart",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      } else {
+        Swal.fire({
+          title: "Already Added to the Cart",
+          text: "This book is already in your cart.",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "OK!",
+        });
+      }
+    },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+    },
+
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  clearCart,
+} = cartSlice.actions;
+
 export default cartSlice.reducer;
